bb82f6ddd9497841dca5f2a016f1df9aaa2c46a1
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2024-09-22 20:26:03 +0700

    add module application configurations

---
+++ AdsManagement/src/Modules/Advertisement/AdsManagement.Modules.Advertisement.Application/Contracts/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace AdsManagement.Modules.Advertisement.Application.Contracts;
+
+public interface ICommand<out TResult> : IRequest<TResult>
+{
+}
+
+public interface ICommand : IRequest
+{
+}

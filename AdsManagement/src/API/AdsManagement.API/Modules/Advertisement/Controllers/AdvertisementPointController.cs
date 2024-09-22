935a95b9f332423be6a7d85c0de02bfa33b17e4c
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2024-09-22 20:25:41 +0700

    add module controllers

---
+++ AdsManagement/src/API/AdsManagement.API/Modules/Advertisement/Controllers/AdvertisementPointController.cs
@@ -0,0 +1,29 @@
+﻿using AdsManagement.API.Common;
+using AdsManagement.Modules.Advertisement.Application.Contracts;
+using AdsManagement.Modules.Advertisement.Application.Queries;
+
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdsManagement.API.Modules.Advertisement.Controllers;
+
+[ApiVersion(ApiVersions.Version1)]
+[ApiController]
+[Route("api/v{version:apiVersion}/ads-point")]
+public class AdvertisementPointController : ControllerBase
+{
+    private readonly IAdvertisementModule _advertisementModule;
+
+    public AdvertisementPointController(IAdvertisementModule advertisementModule)
+    {
+        _advertisementModule = advertisementModule;
+    }
+    
+    [HttpGet("")]
+    public async Task<IActionResult> GetAllAdvertisementPoints()
+    {
+        var user = await _advertisementModule.ExecuteQueryAsync(new GetAllAdvertisementPointsQuery());
+
+        return Ok(user);
+    }
+}
---
+++ AdsManagement/src/API/AdsManagement.API/Modules/Advertisement/Controllers/WardController.cs
@@ -0,0 +1,21 @@
+﻿using AdsManagement.API.Common;
+using AdsManagement.Modules.Advertisement.Application.Contracts;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdsManagement.API.Modules.Advertisement.Controllers;
+
+[ApiVersion(ApiVersions.Version1)]
+[ApiController]
+[Route("api/v{version:apiVersion}/ward")]
+public class WardController : ControllerBase
+{
+    private readonly IAdvertisementModule _advertisementModule;
+
+    public WardController(IAdvertisementModule advertisementModule)
+    {
+        _advertisementModule = advertisementModule;
+    }
+    
+    
+}

0d18a08aa990521e824bd2be9591861a40237ab5
Khang Huynh <89777765+Khaa288@users.noreply.github.com>
2024-09-28 21:34:30 +0700

    add pagination API (#3)

---
+++ AdsManagement/src/Modules/Advertisement/AdsManagement.Modules.Advertisement.Application/Queries/GetAdvertisementPointsPagin/GetAdvertisementPointsPaginQuery.cs
@@ -0,0 +1,23 @@
+﻿using AdsManagement.BuildingBlocks.Application.Common.Pagination;
+using AdsManagement.Modules.Advertisement.Application.Contracts;
+using AdsManagement.Modules.Advertisement.Domain.Entities;
+
+namespace Application.Queries.GetAdvertisementPointsPagin;
+
+public class GetAdvertisementPointsPaginQuery : QueryBase<PagedList<AdvertisementPoint>>
+{
+    public int PageNumber { get; }
+    public int PageSize { get; }
+    public string? LocationType { get; }
+    public string? AdvertisingForm { get; }
+    public bool IsPlanned { get; }
+
+    public GetAdvertisementPointsPaginQuery(int pageNumber, int pageSize, string? locationType, string? advertisingForm, bool isPlanned)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        LocationType = locationType;
+        AdvertisingForm = advertisingForm;
+        IsPlanned = isPlanned;
+    }
+}
--- AdsManagement/src/Modules/Advertisement/AdsManagement.Modules.Advertisement.Domain/Repositories/IAdvertisementPointRepository.cs
+++ AdsManagement/src/Modules/Advertisement/AdsManagement.Modules.Advertisement.Domain/Repositories/IAdvertisementPointRepository.cs
@@ -1,10 +1,13 @@
 ﻿using AdsManagement.Modules.Advertisement.Domain.Entities;
 
+using System.Linq.Expressions;
+
 namespace AdsManagement.Modules.Advertisement.Domain.Repositories;
 
 public interface IAdvertisementPointRepository
 {
     Task<List<AdvertisementPoint>> GetAllAdvertisementPoints();
+    Task<(List<AdvertisementPoint>, int)> GetPaginAdvertisementPoints(int page, int pageSize, Expression<Func<AdvertisementPoint,bool>>? filter);
     Task<AdvertisementPoint> GetAdvertisementPointByPointId(Guid pointId);
     Task<List<AdvertisementPoint>> GetAdvertisementPointsByWardId(int wardId);
     Task<List<AdvertisementPoint>> GetAdvertisementPointsByDistrictId(int districtId);
